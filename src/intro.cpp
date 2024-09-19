bd9f6ae0843cdbfcae04b6395b7a2b337a6a8628
DeStilleGast <3677706+DeStilleGast@users.noreply.github.com>
2024-09-19 20:22:42 +0200

    added before unplug warning page in the intro sequence

--- src/intro.cpp
+++ src/intro.cpp
@@ -67,6 +67,68 @@ void runIntro(M5Canvas &c){
     }
 
 
+    while (M5Cardputer.Keyboard.isPressed()){
+        M5Cardputer.update();
+    }
+
+    c.clear();
+
+    c.drawLine(20, 20, c.width() - 20, 20, TFT_WHITE); 
+    c.drawLine(c.width() - 20, 20, c.width() - 20, 60, TFT_WHITE); 
+    
+
+    c.drawLine(c.width() - 25, 25, c.width() - 120, 25, TFT_WHITE); 
+    c.drawLine(c.width() - 25, 25, c.width() - 25, 60, TFT_WHITE); 
+    c.drawLine(c.width() - 120, 25, c.width() - 120, 60, TFT_WHITE);
+
+    c.drawRightString("M5", c.width() - 125, 22, &fonts::DejaVu40);
+
+    c.fillCircle(c.width() - 32, 32, 5, TFT_RED);
+
+    String scrollMessage = "Ensure that the tool is turned off before unplugging the USB, press [any key] to continue";
+    int messageWidth = c.textWidth(scrollMessage, &fonts::FreeMono9pt7b);
+
+    // c.drawCenterString("Ensure the tool is off", c.width() / 2, c.height() - 60, &fonts::FreeMono9pt7b);
+    // c.drawCenterString("Before disconnecting usb", c.width() / 2, c.height() - 40, &fonts::FreeMono9pt7b);
+    
+    // c.drawCenterString("Press [any] to continue", c.width() / 2, c.height() - 20, &fonts::FreeMono9pt7b);
+    // c.drawLine(c.width() - 40, 40, c.width() - 45, 45, TFT_RED);
+    // c.drawLine(c.width() - 32, 40, c.width() - 32, 50, TFT_RED);
+    // c.drawLine(c.width() - 40, 32, c.width() - 50, 32, TFT_RED);
+
+
+    c.pushSprite(0, 0);
+    // M5Cardputer.Display.setEpdMode(epd_mode_t::epd_fastest);
+
+    unsigned long scrollOffset = millis();
+    while(true){
+        M5Cardputer.update();
+
+        c.fillRect(0, c.height() - 40, c.width(), c.fontHeight(&fonts::FreeMono9pt7b), TFT_BLACK);
+        c.drawString(scrollMessage, c.width() - ((millis() - scrollOffset) / 15 % (messageWidth + c.width())), c.height() - 40, &fonts::FreeMono9pt7b);
+
+        c.drawLine(c.width() - 40, 40, c.width() - 45, 45, millis() % 1000 > 500 ? TFT_RED : TFT_BLACK);
+        c.drawLine(c.width() - 32, 40, c.width() - 32, 50, millis() % 1000 > 500 ? TFT_RED : TFT_BLACK);
+        c.drawLine(c.width() - 40, 32, c.width() - 50, 32, millis() % 1000 > 500 ? TFT_RED : TFT_BLACK);
+
+        c.waitDisplay();
+        c.pushSprite(0, 0);
+
+        if(M5Cardputer.BtnA.isPressed()) {
+            return;
+        }
+
+        if(M5Cardputer.Keyboard.isPressed()){
+            break;
+        }
+    }
+
+    
+    while (M5Cardputer.Keyboard.isPressed()){
+        M5Cardputer.update();
+    }
+
+
     c.clear();
 
     int topPosX = c.width()/2 - 60;
@@ -88,7 +150,7 @@ void runIntro(M5Canvas &c){
 
     c.fillArc(topPosX + 95, topPosY + 24, 10, 13, 0, 360, TFT_RED);
 
-    c.drawCenterString("Press G0 to go back", c.width() / 2, c.height() - 20, &fonts::FreeMono9pt7b);
+    c.drawCenterString("Press G0 for the menu", c.width() / 2, c.height() - 20, &fonts::FreeMono9pt7b);
 
     c.pushSprite(0, 0);
 
